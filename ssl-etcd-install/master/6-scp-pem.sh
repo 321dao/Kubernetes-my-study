3463ec341e22846a9133050ce49ddba63c57996f
hx6007 <32677621+hx6007@users.noreply.github.com>
2018-08-29 20:16:52 +0800

    Update 6-scp-pem.sh

--- ssl-etcd-install/master/6-scp-pem.sh
+++ ssl-etcd-install/master/6-scp-pem.sh
@@ -5,5 +5,5 @@ scp -r /opt/kubernetes/bin/ root@192.168.224.143:/opt/kubernetes/
 scp -r /opt/kubernetes/bin/ root@192.168.224.144:/opt/kubernetes/
 scp -r /opt/kubernetes/ssl/ root@192.168.224.144:/opt/kubernetes/
 scp -r /opt/kubernetes/ssl/ root@192.168.224.143:/opt/kubernetes/
-scp /usr/lib/systemd/system/etcd.service root@192.168.224.143:/usr/lib/systemd/system/
-scp /usr/lib/systemd/system/etcd.service root@192.168.224.144:/usr/lib/systemd/system/
+#scp /usr/lib/systemd/system/etcd.service root@192.168.224.143:/usr/lib/systemd/system/
+#scp /usr/lib/systemd/system/etcd.service root@192.168.224.144:/usr/lib/systemd/system/
