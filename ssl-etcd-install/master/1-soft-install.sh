1e46911dd5c69602ea25df7df7e116c9b1dbd503
hx <dfsf@126.com>
2018-08-29 16:33:18 +0800

    mkdir dir

---
+++ ssl-etcd-install/master/1-soft-install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+yum -y install wget lrzsz mlocate deltarpm 
+updatedb
+cd /etc/yum.repos.d/
+mv /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.backup 
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+yum clean all
+yum makecache
+hostname k8s-master
+echo k8s-master > /etc/hostname
+
+sed -i 's/enforcing/disabled/g' /etc/selinux/config
+setenforce 0
+iptables -F
+iptables -t nat -F
+iptables -I FORWARD -s 0.0.0.0/0 -d 0.0.0.0/0 -j ACCEPT  
+iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 2379 -j ACCEPT
+iptables -A INPUT -p tcp -m state --state NEW -m tcp --dport 2380 -j ACCEPT
+
+
