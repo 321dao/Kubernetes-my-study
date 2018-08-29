11308a4e75091505aa919229ee285d5637b9e6ef
hx <dfsf@126.com>
2018-08-29 16:54:56 +0800

    fix path

--- ssl-etcd-install/master/2-ssl-install.sh
+++ ssl-etcd-install/master/2-ssl-install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 mkdir ssl && cd ssl
-wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 
-wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
-wget https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
-chmod +x * 
-mv cfssl-certinfo_linux-amd64  /usr/local/bin/cfssl-certinfo   
-mv cfssljson_linux-amd64     /usr/local/bin/cfssljson
-mv  cfssl_linux-amd64       /usr/local/bin/cfssl
+#wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 
+#wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
+#wget https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
+#chmod +x * 
+mv ../soft/cfssl-certinfo_linux-amd64  /usr/local/bin/cfssl-certinfo   
+mv ../soft/cfssljson_linux-amd64     /usr/local/bin/cfssljson
+mv ../soft/cfssl_linux-amd64       /usr/local/bin/cfssl
 $(which sh) ../certificate.sh
 ls | grep -v pem | xargs -i rm {}
 mkdir -p /opt/kubernetes/{bin,cfg,ssl}
