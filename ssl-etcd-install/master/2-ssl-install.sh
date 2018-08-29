74798e832383f4a8aeed69c58b29ee1c41b94b60
huagxin <462563017@qq.com>
2018-08-29 19:52:47 +0800

    fix path

--- ssl-etcd-install/master/2-ssl-install.sh
+++ ssl-etcd-install/master/2-ssl-install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir ssl && cd ssl
+#mkdir ssl && cd ssl
 #wget https://pkg.cfssl.org/R1.2/cfssl_linux-amd64 
 #wget https://pkg.cfssl.org/R1.2/cfssljson_linux-amd64
 #wget https://pkg.cfssl.org/R1.2/cfssl-certinfo_linux-amd64
@@ -7,7 +7,8 @@ mkdir ssl && cd ssl
 mv ../soft/cfssl-certinfo_linux-amd64  /usr/local/bin/cfssl-certinfo   
 mv ../soft/cfssljson_linux-amd64     /usr/local/bin/cfssljson
 mv ../soft/cfssl_linux-amd64       /usr/local/bin/cfssl
-$(which sh) ../certificate.sh
+ cd ssl
+$(which sh) certificate.sh
 ls | grep -v pem | xargs -i rm {}
 mkdir -p /opt/kubernetes/{bin,cfg,ssl}
 cp *pem /opt/kubernetes/ssl
