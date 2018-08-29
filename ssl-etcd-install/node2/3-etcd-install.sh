a6a51b09035615612837e45d3c9d33e5561c3cb7
hx6007 <32677621+hx6007@users.noreply.github.com>
2018-08-29 20:11:11 +0800

    Update 3-etcd-install.sh

--- ssl-etcd-install/node2/3-etcd-install.sh
+++ ssl-etcd-install/node2/3-etcd-install.sh
@@ -2,7 +2,7 @@
 cp etcd /opt/kubernetes/cfg/
 cp etcd.service /usr/lib/systemd/system/
 #wget https://github.com/etcd-io/etcd/releases/download/v3.3.9/etcd-v3.3.9-linux-amd64.tar.gz
-mv ../soft/etcd-v3.3.9-linux-amd64.tar.gz ./
+cp ../soft/etcd-v3.3.9-linux-amd64.tar.gz ./
 tar xvf etcd-v3.3.9-linux-amd64.tar.gz
 cd etcd-v3.3.9-linux-amd64
 mv etcd etcdctl /opt/kubernetes/bin/
